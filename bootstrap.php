505d77c8fee9669020352d8317eddb6abddeff4c
Richard Mahn <online@richmahn.com>
2015-06-23 08:22:37 -0600

    added bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Door43 boostrap script
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Richard Mahn <richard_mahn@wycliffeassociates.org>
+ *
+ * @global Input $INPUT
+ */
+
+echo "Staring bootstrap...";
